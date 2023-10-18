39d97b124c310a517464125c1c59b456f14c7731
Константин Суфтин <konstantin.suftin@gmail.com>
2023-10-18 12:29:22 +0300

    Initial commit on branch feature/MARKET-1 Базовый шаблон создан.

--- src/market/market/settings.py
+++ src/market/market/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'dev_app.apps.DevAppConfig',
     'market.users_permissions.apps.UsersPermissionsConfig',
 ]
 
@@ -125,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ('static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
