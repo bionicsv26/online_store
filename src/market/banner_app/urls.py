be98f333da3962253064980f685f6d2f8c963dc4
konstantin.suftin@gmail.com <lab741593>
2023-10-20 11:15:56 +0300

    feature/MARKET-6 1. Добавлен слайдер на страницу index.html

--- src/market/banner_app/urls.py
+++ src/market/banner_app/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
-from .views import test_view_func
+from .views import (
+    IndexTemplateView,
+)
+
+app_name = 'banner_app'
+
+urlpatterns = [
+    path('index/', IndexTemplateView.as_view(), name='index'),
+]
--- src/market/banner_app/views.py
+++ src/market/banner_app/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import (
     HttpRequest,
-    HttpResponse,
 )
-# Create your views here.
+from django.views.generic import (
+    TemplateView,
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+)
+from .models import BannerSlider
+
 
 def test_view_func(request: HttpRequest):
     return render(request, 'banner_app/test.html')
+
+class IndexTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = "banner_app/index.html"
+    #context_object_name = 'purposes'
+
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexTemplateView, self).get_context_data(**kwargs)
+
+        context['banners_sliders'] = BannerSlider.objects.all()
+
+        return context
