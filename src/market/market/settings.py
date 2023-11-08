a298dd1c81957251b78f1d4d8fd7df3c07649d71
Андрей Сорокожердьев <sorokozherdyev@gmail.com>
2023-11-08 12:15:28 +0300

    удален дубляж

--- src/market/market/settings.py
+++ src/market/market/settings.py
@@ -52,8 +52,6 @@ INSTALLED_APPS = [
     'market.products',
     'market.categories',
     'market.banner_app.apps.BannerAppConfig',
-    'market.products.apps.ProductsConfig',
-    'market.categories.apps.CategoriesConfig',
     'market.settingsapp.apps.SettingsappConfig',
     'market.orders',
     'market.payment',
