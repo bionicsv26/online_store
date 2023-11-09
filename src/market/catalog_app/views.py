2d0606b612622399cc0e6fb793d19318ce4c2baa
konstantin.suftin@gmail.com <lab741593>
2023-11-09 10:01:42 +0300

    feature/MARKET-8 1. Добавил пагинацию на страницу каталога

--- src/market/catalog_app/views.py
+++ src/market/catalog_app/views.py
@@ -1,6 +1,6 @@
 from market.categories.mixins import MenuMixin
 from django.views.generic import (
-    TemplateView,
+    ListView,
 )
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
@@ -13,20 +13,17 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class CatalogTemplateView(LoginRequiredMixin, TemplateView, MenuMixin):
+
+class CatalogTemplateView(LoginRequiredMixin, MenuMixin, ListView):
     template_name = "catalog_app/catalog.html"
-    paginate_by = 5
+    model = Product
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(CatalogTemplateView, self).get_context_data(**kwargs)
+        log.debug("Запуск рендеренга CatalogOldTemplateView")
 
-        log.debug("Запуск рендеренга ProductDetailView")
-        products = Product.objects.all()
-        print("products", products)
-        for product in products:
-            print("Object product", product, type(product))
-            for category in product.category.all():
-                print("Category ", type(category), category.name)
-        context['products'] = products
-        log.debug("Контекст готов. Продукты отсортированы", context)
+        for item in context.items():
+            print("Item ", item)
+        log.debug("Контекст готов. Продукты отсортированы")
         return context
