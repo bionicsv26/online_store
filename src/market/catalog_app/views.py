475fa43d2c2c4411b1742bb236f841331a16f12f
konstantin.suftin@gmail.com <lab741593>
2023-11-13 23:04:23 +0300

    feature/MARKET-8 Сделать: 1. Дополнил продукты продавцов и продавцов 2. Поменял в катологе отображение продуктов на отображение предложений 3. настроил 2 из 4 сортировок

--- src/market/catalog_app/views.py
+++ src/market/catalog_app/views.py
@@ -5,10 +5,12 @@ from django.views.generic import (
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
 )
-from market.products.models import Product
+from market.products.models import Product, ProductFeedback
+from .models import SellerProduct
 
 from market.settingsapp.models import CacheTime
 import logging
+from django.db.models import Count
 
 log = logging.getLogger(__name__)
 
@@ -16,19 +18,30 @@ log = logging.getLogger(__name__)
 
 class CatalogTemplateView(LoginRequiredMixin, MenuMixin, ListView):
     template_name = "catalog_app/catalog.html"
-    model = Product
-    paginate_by = 3
+    model = SellerProduct
+    paginate_by = 8
 
     def get_queryset(self):
         print("self.request.GET", self.request.GET)
-        order_by = self.request.GET.get('order_by', 'name')
-        print("order_by", order_by, type(order_by))
-        return Product.objects.order_by(order_by)
+        if "order_by" in self.request.GET:
+            print(" ordered_by in self.request.GET")
+            order_by = self.request.GET.get('order_by', 'price')
+            return SellerProduct.objects.order_by(order_by)
+        elif "feedback" in self.request.GET:
+            #qwery = SellerProduct.objects.annotate(num_feedbacks=Count('product__productfeedback__feedback_text')).order_by("num_feedbacks")
+            qwery = SellerProduct.objects.annotate(num_feedbacks=Count('discount')).order_by("-num_feedbacks")
+            print("qwery", qwery)
+            return qwery
+        else:
+            print(" Ordered_by NOT in self.request.GET")
+            return SellerProduct.objects.order_by('price')
 
     def get_context_data(self, **kwargs):
         context = super(CatalogTemplateView, self).get_context_data(**kwargs)
         log.debug("Запуск рендеренга CatalogOldTemplateView")
 
+        feedbacks_count = ProductFeedback.objects.annotate(num_feedbacks=Count('feedback_text'))
+        print("feedbacks_count", feedbacks_count)
         for item in context.items():
             print("Item ", item)
         log.debug("Контекст готов. Продукты отсортированы")
