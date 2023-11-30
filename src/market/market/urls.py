1f11a0e4fc9842bd5ad3522739bc4f706e457562
Андрей Сорокожердьев <asorokozherdyev@gmail.com>
2023-11-30 14:49:25 +0300

    Задача MARKET-9 реализована

--- src/market/market/urls.py
+++ src/market/market/urls.py
@@ -14,10 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -30,7 +27,8 @@ urlpatterns = [
     path('profile/orders/', include('market.orders.urls')),
     path('profile/orders/<int:pk>/', include('market.payment.urls')),
     path('bank/', include('market.paymentsystem.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('profiles/', include('market.profiles.urls')),
+    path('sellers/', include('market.sellers.urls')),
 ]
 
 if settings.DEBUG:
@@ -39,4 +37,8 @@ if settings.DEBUG:
     )
     urlpatterns.extend(
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
+
+    urlpatterns.append(
+        path('__debug__/', include('debug_toolbar.urls')),
     )
---
+++ src/market/sellers/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from market.products.models import Product
+
+
+def seller_logo_directory_path(instance: 'Seller', filename) -> str:
+    return f'images/sellers/{instance.slug}/logo/{filename}'
+
+
+class Seller(models.Model):
+    class Meta:
+        ordering = 'name',
+        verbose_name = 'продавец'
+        verbose_name_plural = 'продавцы'
+
+    name = models.CharField(max_length=256, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    logo = models.ImageField(null=True, unique=True, upload_to=seller_logo_directory_path, verbose_name='лого')
+    archived = models.BooleanField(default=False, verbose_name='архивация продавца')
+    slug = models.SlugField(unique=True, verbose_name='slug')
+
+    def __str__(self):
+        return f'Продавец "{self.name}"'
+
+
+class SellerProduct(models.Model):
+    class Meta:
+        verbose_name = 'продукт продавца'
+        verbose_name_plural = 'продукты продавца'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='seller_products', verbose_name='продукт')
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='seller_products', verbose_name='продавец')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    stock = models.IntegerField(default=0, verbose_name='сколько в наличии')
+    discount = models.ForeignKey('Discount', on_delete=models.PROTECT, null=True, related_name='seller_products', verbose_name='скидка')
+
+    def __str__(self):
+        return f'Продукт продавца "{self.product.name}"'
+
+
+class Discount(models.Model):
+    class Meta:
+        ordering = 'name',
+        verbose_name = 'скидка'
+        verbose_name_plural = 'скидки'
+
+    name = models.CharField(max_length=256, verbose_name='название')
+    value = models.IntegerField(default=0, verbose_name='скидка в %')
+
+    def __str__(self):
+        return self.name
