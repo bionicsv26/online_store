a6a84af7d425db1a517234226380eaba9b9f8f86
konstantin.suftin@gmail.com <lab741593>
2023-12-09 21:25:27 +0300

    feature/MARKET-8 Все замечания Андрея отработаны.

--- src/market/catalog_app/views.py
+++ src/market/catalog_app/views.py
@@ -19,24 +19,23 @@ class CatalogTemplateView(LoginRequiredMixin, MenuMixin, ListView):
     paginate_by = 8
 
     def get_queryset(self):
-        if "order_by" in self.request.GET:
-            order_by = self.request.GET.get('order_by', 'price')
+        order_by = self.request.GET.get('order_by', 'price')
+        if order_by == 'price' or order_by == '-created_at':
             return SellerProduct.objects.order_by(order_by)
-        elif "feedback" in self.request.GET:
+        elif order_by == "feedback":
             queryset = SellerProduct.objects.annotate(
                 num_feedbacks=Count('product__feedback__feedback_text')).order_by("-num_feedbacks")
             return queryset
-        elif "rating" in self.request.GET:
+        elif order_by == "rating":
             queryset = SellerProduct.objects.annotate(
                 num_of_sale=Count('orders', filter=Q(orders__status__name='payed'))
             ).order_by("-num_of_sale")
             return queryset
-        else:
-            log.debug("Ordered_by NOT in self.request.GET")
-            return SellerProduct.objects.order_by('-price')
 
     def get_context_data(self, **kwargs):
         context = super(CatalogTemplateView, self).get_context_data(**kwargs)
+        order_by = self.request.GET.get('order_by', 'price')
+        context['order_by'] = order_by
         log.debug("Запуск рендеренга CatalogOldTemplateView")
         log.debug("Контекст готов. Продукты отсортированы")
         return context
