916e197984d6b3d45081c2c70c40bd5e33340893
konstantin.suftin@gmail.com <lab741593>
2023-12-15 23:15:01 +0300

    feature/MARKET-12 Реализовано отображение отзывов на товар

--- src/market/products/views.py
+++ src/market/products/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 
 from market.categories.mixins import MenuMixin
-from market.products.models import Product
+from market.banner_app.mixins import BannerSliderMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+)
+from market.products.models import Product, ProductFeedback
+import logging
+
+log = logging.getLogger(__name__)
 
 
-class ProductDetailView(MenuMixin, DetailView):
+class ProductDetailView(MenuMixin, BannerSliderMixin, LoginRequiredMixin, DetailView):
     template_name = 'products/product_details.html'
     model = Product
     context_object_name = 'product'
     slug_url_kwarg = 'product_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['product_feedbacks'] = ProductFeedback.objects.filter(product=self.object)
+        log.debug("Запуск рендеренга ProductDetailView")
+        log.debug("Контекст для ProductDetailView готов. ")
+        return context
