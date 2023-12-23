94ba6271e33d4e1aa959e58ee6d1a1dde7a637e2
konstantin.suftin@gmail.com <lab741593>
2023-12-23 20:58:14 +0300

    feature/MARKET-13 Создал темплейты account.html and profile.html Добавил перекрестные ссылки

--- src/market/profiles/views.py
+++ src/market/profiles/views.py
@@ -2,7 +2,14 @@ from django.contrib import messages
 from django.views import View
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
-
+from market.banner_app.mixins import BannerSliderMixin
+from market.categories.mixins import MenuMixin
+from django.views.generic import (
+    TemplateView,
+)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+)
 
 class RegisterView(View):
     form_class = UserRegistrationForm
@@ -25,3 +32,10 @@ class RegisterView(View):
             return redirect(to='/')
         
         return render(request, self.template_name, {'form': form})
+
+class AccountTemplateView(LoginRequiredMixin, TemplateView, BannerSliderMixin, MenuMixin):
+    template_name = "profiles/account.html"
+
+
+class ProfileTemplateView(LoginRequiredMixin, TemplateView, BannerSliderMixin, MenuMixin):
+    template_name = "profiles/profile.html"
