bff9b9180f0007d72a5ef74ed9835f190a32827e
konstantin.suftin@gmail.com <lab741593>
2023-12-24 14:19:17 +0300

    feature/MARKET-13 Доработаны шаблоны + добавлена верстка для показа 3 последних просмотренных товаров + ссылка на весь список

--- src/market/profiles/views.py
+++ src/market/profiles/views.py
@@ -10,6 +10,9 @@ from django.views.generic import (
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
 )
+import logging
+
+log = logging.getLogger(__name__)
 
 class RegisterView(View):
     form_class = UserRegistrationForm
@@ -36,6 +39,13 @@ class RegisterView(View):
 class AccountTemplateView(LoginRequiredMixin, TemplateView, BannerSliderMixin, MenuMixin):
     template_name = "profiles/account.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        order_by = self.request.GET.get('order_by', 'price')
+        context['account_user_name'] = self.request.user
+        log.debug("Запуск рендеренга AccountTemplateView")
+        log.debug("Контекст готов и передан на страницу account.html")
+        return context
 
 class ProfileTemplateView(LoginRequiredMixin, TemplateView, BannerSliderMixin, MenuMixin):
     template_name = "profiles/profile.html"
