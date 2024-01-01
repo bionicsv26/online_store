a059de6bb323300fc3d619cb5519755e659d3860
konstantin.suftin@gmail.com <lab741593>
2024-01-01 10:10:23 +0300

    feature/MARKET-13 Добавлена визуализация просмотренных товаров в каталоге

--- src/market/catalog_app/views.py
+++ src/market/catalog_app/views.py
@@ -9,9 +9,15 @@ from market.sellers.models import SellerProduct
 from django.db.models import Q
 import logging
 from django.db.models import Count
+from market.browsing_history_app.models import ProductBrowsingHistory
 
 log = logging.getLogger(__name__)
 
+def list_of_product_names(queryset):
+    result_list = list()
+    for i_product in queryset:
+        result_list.append(i_product.product.name)
+    return result_list
 
 class CatalogTemplateView(LoginRequiredMixin, MenuMixin, ListView):
     template_name = "catalog_app/catalog.html"
@@ -36,6 +42,8 @@ class CatalogTemplateView(LoginRequiredMixin, MenuMixin, ListView):
         context = super(CatalogTemplateView, self).get_context_data(**kwargs)
         order_by = self.request.GET.get('order_by', 'price')
         context['order_by'] = order_by
+        browsing_history = ProductBrowsingHistory.objects.filter(user=self.request.user).all()
+        context['browsing_history'] = list_of_product_names(browsing_history)
         log.debug("Запуск рендеренга CatalogOldTemplateView")
         log.debug("Контекст готов. Продукты отсортированы")
         return context
