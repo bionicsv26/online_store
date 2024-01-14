e78502dab07beba5de52f3df773531e4e4dcdea8
Roman <pereyaslavskiy89@mail.ru>
2024-01-14 23:30:25 +0900

    Реализована корзина посредством сессии

--- src/market/products/views.py
+++ src/market/products/views.py
@@ -17,6 +17,7 @@ from market.cart.cart import Cart
 
 log = logging.getLogger(__name__)
 from typing import Any
+from django.db.models import Min
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
@@ -24,6 +25,7 @@ from django.views.generic import DetailView, ListView
 
 from market.categories.mixins import MenuMixin
 from market.products.models import Product
+from market.sellers.models import SellerProduct
 from market.cart.cart import Cart
 
 
@@ -85,10 +87,22 @@ class ProductDetailView(MenuMixin, BannerSliderMixin, LoginRequiredMixin, Detail
         return context
 
     def get_queryset(self) -> QuerySet[Any]:
-        return Product.objects.prefetch_related('seller_products')
+        return super().get_queryset().prefetch_related('seller_products')
     
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        amount = request.GET.get('amount')
+        response = super().get(request, *args, **kwargs)
         cart = Cart(request)
-        
-        return super().get(request, *args, **kwargs)
+        amount = request.GET.get('amount')
+        seller_product_id = request.GET.get('seller_product_pk')
+
+        if seller_product_id is None:
+            product = self.model.objects.get(slug=kwargs['product_slug'])
+            seller_product = product.seller_products.order_by('price').first()
+        else:
+            seller_product = SellerProduct.objects.get(id=seller_product_id)
+            amount = 1
+
+        if amount or seller_product_id:
+            cart.add(seller_product, amount)
+
+        return response
