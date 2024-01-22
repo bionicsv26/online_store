2ae8edc908673367643282457de19505bf63666b
Roman <pereyaslavskiy89@mail.ru>
2024-01-23 01:16:46 +0900

    Рабочая ветка с реализованной корзиной

--- src/market/cart/urls.py
+++ src/market/cart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'cart'
+app_name = 'market.cart'
 
 urlpatterns = [
     path('', views.CartDetailsView.as_view(), name='cart_details'),
