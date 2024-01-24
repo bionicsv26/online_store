9ec4f532379edee6a5ad0879d9d4455c95b87376
konstantin.suftin@gmail.com <lab741593>
2024-01-24 09:30:08 +0300

    feature/MARKET-22 1. Написана форма 2. Начата подготовка шаблона

--- src/market/profiles/forms.py
+++ src/market/profiles/forms.py
@@ -1,11 +1,13 @@
+import re
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth import password_validation, authenticate
 from django.contrib.auth.models import User
+from .models import Profile
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRegistrationForm(forms.ModelForm): 
+class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
